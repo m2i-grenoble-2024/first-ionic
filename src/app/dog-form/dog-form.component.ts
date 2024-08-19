6e22fdb799a961a8d61283f6d689939cf2f3e05a
Jean Demel <demeljean@hotmail.fr>
2024-08-19 16:32:18 +0200

    dog form

--- src/app/dog-form/dog-form.component.ts
+++ src/app/dog-form/dog-form.component.ts
@@ -1,11 +1,14 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Dog } from '../entities';
+import { IonInput, IonDatetime, IonDatetimeButton, IonModal } from "@ionic/angular/standalone";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-dog-form',
   templateUrl: './dog-form.component.html',
   styleUrls: ['./dog-form.component.scss'],
   standalone: true,
+  imports: [IonModal, IonDatetimeButton, IonDatetime, IonInput, FormsModule]
 })
 export class DogFormComponent  implements OnInit {
   @Input()
