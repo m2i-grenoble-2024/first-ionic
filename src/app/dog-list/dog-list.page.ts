eae49fd0d7a39af92f9ecdb2dc5c157eb7f7df3f
Jean Demel <demeljean@hotmail.fr>
2024-08-20 15:35:37 +0200

    fab + modal

--- src/app/dog-list/dog-list.page.ts
+++ src/app/dog-list/dog-list.page.ts
@@ -1,23 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonFab, IonIcon, IonFabButton, IonModal, IonButton, IonButtons } from '@ionic/angular/standalone';
 import { Dog } from '../entities';
 import { DogService } from '../dog.service';
 import { DogCardComponent } from '../dog-card/dog-card.component';
+import { addIcons } from 'ionicons';
+import { add } from 'ionicons/icons';
+import { DogFormComponent } from "../dog-form/dog-form.component";
 
 @Component({
   selector: 'app-dog-list',
   templateUrl: './dog-list.page.html',
   styleUrls: ['./dog-list.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, DogCardComponent]
+  imports: [IonButtons, IonButton, IonModal, IonFabButton, IonIcon, IonFab, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, DogCardComponent, DogFormComponent]
 })
 export class DogListPage implements OnInit {
+  // @ViewChild(IonModal) 
+  // modal!:IonModal;
 
   dogs:Dog[] = [];
 
-  constructor(private dogService:DogService) { }
+  constructor(private dogService:DogService) {
+    addIcons({add})
+
+   }
 
   ngOnInit() {
     this.dogService.fetchAll().subscribe(data => this.dogs = data);
